3085c3ca354fe6c67857b687a9559669d65886af
markwartman1 <markwartman1@gmail.com>
2020-06-25 15:00:19 -0500

    creation test Passing for CartItemServiceContainer

--- revature-cart-backend/src/test/java/com/revature/cart/service/container/CartItemServiceContainerTest.java
+++ revature-cart-backend/src/test/java/com/revature/cart/service/container/CartItemServiceContainerTest.java
@@ -21,7 +21,7 @@ class CartItemServiceContainerTest {
 
 	@Autowired
 	CartItemServiceContainer cisc;
-	
+
 	@Autowired
 	CartServiceContainer csc;
 
@@ -29,30 +29,20 @@ class CartItemServiceContainerTest {
 	void test_createCartItem() {
 
 		CartItem cartItem = new CartItem();
-		List<CartItem> cartItemListEmpty = new ArrayList<>();
 
-		Cart cart = new Cart();
-		cart.setCartId(1);
-		cart.setUserId(1);
-		// cart.setName("need a name ?");
-		cart.setCartItems(cartItemListEmpty);
-
-
-		//cartItem.setCartItemId(1);
-		cartItem.setCart(cart);
 		cartItem.setProductId(1);
 		cartItem.setQuantity(1);
 
 		cisc.createCartItem(cartItem);
-		System.out.println("\n\nHelloWorld! " + cart.toString());
-		System.out.println("\n\nHelloWorld! " + cart.getCartId());
+		System.out.println("\n\nHERE IS DATA: ");
+		System.out.println("cartItem.getCartItemId(): " + cartItem.getCartItemId());
+		System.out.println("cartItem.getQuantity(): " + cartItem.getQuantity());
 		
-		
+
 		List<CartItem> cartItemListFromDB = new ArrayList<>();
-		//CartItem cartItem1 = cisc.getCartItemById(cartItem.getCartItemId());
-		
+
 		// fill List with getCartItemById (the Cart's id)
-		cartItemListFromDB.add(cisc.getCartItemById(cart.getCartId()));
+		cartItemListFromDB.add(cisc.getCartItemById(cartItem.getCartItemId()));
 
 		assertEquals(1, cartItemListFromDB.size());
 	}
@@ -82,60 +72,40 @@ class CartItemServiceContainerTest {
 //		cisc.deleteCartItemById(cartItem.getCartItemId());
 //		assertEquals(before, (ciFromDB.size() + 1));
 //	}
-	
+
 	@Test
 	void test_deleteCartItem() {
 
+		
+		List<CartItem> listOfCartItemsFromDB = new ArrayList<>();
+		listOfCartItemsFromDB = cisc.getAllCartItems();
+
 		CartItem cartItem = new CartItem();
-		List<CartItem> cartItemListEmpty = new ArrayList<>();
-
-		Cart cart = new Cart();
-		cart.setCartId(1);
-		cart.setUserId(1);
-		// cart.setName("need a name ?");
-		cart.setCartItems(cartItemListEmpty);
-
-		//cartItem.setCartItemId(1);
-		csc.createCart(cart);
-		
-		
-		cartItem.setCart(cart);
+		// cartItem.setCartItemId(1);
 		cartItem.setProductId(1);
 		cartItem.setQuantity(1);
 
 		cisc.createCartItem(cartItem);
-		List<CartItem> ciFromDB = new ArrayList<>();
-		ciFromDB.add(cisc.getCartItemById(cart.getCartId()));
-		int before = ciFromDB.size();
+		System.out.println("\n\nHERE IS DATA: ");
+		System.out.println("cartItem.getCartItemId(): " + cartItem.getCartItemId());
+		System.out.println("cartItem.getQuantity(): " + cartItem.getQuantity());
 		
+		// after Creation check size
+		int before = listOfCartItemsFromDB.size();
+		
+
+		//List<CartItem> cartItemListFromDB = new ArrayList<>();
+		// CartItem cartItem1 = cisc.getCartItemById(cartItem.getCartItemId());
+		
+
+		// fill List with getCartItemById (the Cart's id)
+		//cartItemListFromDB.add(cisc.getCartItemById(cartItem.getCartItemId()));
+		
+
+		
+		// after deletion check size after adding 1 to it to see if it is equal to Before
 		cisc.deleteCartItemById(cartItem.getCartItemId());
-		assertEquals(before, (ciFromDB.size() + 1));
+		assertEquals(before, (listOfCartItemsFromDB.size() + 1));
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
