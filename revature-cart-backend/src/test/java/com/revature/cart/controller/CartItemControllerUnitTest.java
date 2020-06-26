f95ed76fc366fa8a000e67dfc6c0d54ef15cdd38
Ben Knaak <knaak010@umn.edu>
2020-06-26 13:02:34 -0500

    Update cart item controller unit test

--- revature-cart-backend/src/test/java/com/revature/cart/controller/CartItemControllerUnitTest.java
+++ revature-cart-backend/src/test/java/com/revature/cart/controller/CartItemControllerUnitTest.java
@@ -1,27 +1,29 @@
 package com.revature.cart.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.cart.model.Cart;
 import com.revature.cart.model.CartItem;
 import com.revature.cart.service.container.CartItemServiceContainer;
 
-@ExtendWith(SpringExtension.class)
-@AutoConfigureJsonTesters
+@RunWith(SpringRunner.class)
 @WebMvcTest(CartItemController.class)
 public class CartItemControllerUnitTest {
 	
@@ -31,18 +33,25 @@ public class CartItemControllerUnitTest {
 	@MockBean
 	private CartItemServiceContainer cis;
 	
-	@Autowired
-	private JacksonTester<CartItem> jsonCartItem;
+	CartItem mockCI = new CartItem(1, new Cart(), 1, 1);
+	ObjectMapper om = new ObjectMapper();
 	
 	@Test
 	public void test_canCreateCartItem() throws Exception {
-		// when
-		MockHttpServletResponse response = mvc.perform(
-			post("/cartitem").contentType(MediaType.APPLICATION_JSON).content(
-					jsonCartItem.write(new CartItem(1, new Cart(), 1, 1)).getJson()
-			)).andReturn().getResponse();
-			
-		// then
-		assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+
+		String ciJson = om.writeValueAsString(mockCI);
+		
+		Mockito.when(cis.createCartItem(Mockito.any(CartItem.class))).thenReturn(mockCI);
+		Mockito.when(cis.updateCartItem(Mockito.any(CartItem.class))).thenReturn(mockCI);
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.post("/cartitem")
+				.accept(MediaType.APPLICATION_JSON).content(ciJson)
+				.contentType(MediaType.APPLICATION_JSON);
+		
+		MvcResult result = mvc.perform(requestBuilder).andReturn();
+		MockHttpServletResponse response = result.getResponse();
+		assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+		assertEquals("http://localhost:9999/cartitem", response.getHeader(HttpHeaders.LOCATION));
 	}
 }
