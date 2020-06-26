e059aea59112f483bc27071313937951d533ec06
markwartman1 <markwartman1@gmail.com>
2020-06-26 10:39:42 -0500

    boiler-plate Mockito testing class

---
+++ revature-cart-backend/src/test/java/com/revature/cart/controller/CartControllerTest.java
@@ -0,0 +1,22 @@
+package com.revature.cart.controller;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class CartControllerTest {
+	
+	private MockMvc mockMvc;
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
