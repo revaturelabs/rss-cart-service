a071cdce8ce5c87f7fd1272510881a91ccd1b285
knoxiiw <64433273+knoxiiw@users.noreply.github.com>
2020-06-26 14:46:25 -0700

    Update CartController.java

--- revature-cart-backend/src/main/java/com/revature/cart/controller/CartController.java
+++ revature-cart-backend/src/main/java/com/revature/cart/controller/CartController.java
@@ -3,6 +3,7 @@ package com.revature.cart.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.revature.cart.model.Cart;
 import com.revature.cart.service.container.CartServiceContainer;
 
 @RestController
+@CrossOrigin
 public class CartController {
 	
 	/**
