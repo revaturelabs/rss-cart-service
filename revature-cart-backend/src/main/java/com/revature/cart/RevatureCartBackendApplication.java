2ec20acde321785d9924f7c88a4b17f4a61ae0cf
thecheatisgod@gmail.com <thecheatisgod@gmail.com>
2020-11-19 08:11:44 -0700

    finalize for demo

--- revature-cart-backend/src/main/java/com/revature/cart/RevatureCartBackendApplication.java
+++ revature-cart-backend/src/main/java/com/revature/cart/RevatureCartBackendApplication.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 
@@ -16,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @RibbonClient(name = "rss-cart-service")
+@EnableDiscoveryClient
 public class RevatureCartBackendApplication {
 
 	public static void main(String[] args) {
